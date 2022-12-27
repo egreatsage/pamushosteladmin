f2de54acdb23b143c67059a2ca11f27a74810600
GreatSage <michaelcull9715@gmail.com>
2022-12-28 00:30:06 +0300

    fc

--- src/componets/Login.jsx
+++ src/componets/Login.jsx
@@ -24,13 +24,13 @@ const Login = () => {
     <div>
       <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
       
-            <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-rose-600/40   lg:max-w-xl">
-                <h1 className="text-3xl  text-center text-indigo-700 uppercase ">
+            <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-rose-900/40   lg:max-w-xl">
+                <h1 className="text-3xl mb-10 text-center text-indigo-700 uppercase ">
                    Admin Sign In
                 </h1>
                 {error && <Alert color='error'>{error}</Alert>}
                 <form className="mt-6" onSubmit={handleSubmit} >
-                    <div className="mb-2">
+                    <div className="mb-7">
                         <label
                             for="email"
                             className="block text-sm font-semibold text-gray-800"
@@ -39,10 +39,10 @@ const Login = () => {
                         </label>
                         <input
                             type="email"
-                            className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                            className="block w-full px-4 py-2 mt- text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
                             onChange={e=>setEmail(e.target.value)}/>
                     </div>
-                    <div className="mb-2">
+                    <div className="mb-4">
                         <label
                             for="password"
                             className="block text-sm font-semibold text-gray-800"
@@ -54,13 +54,8 @@ const Login = () => {
                             className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
                             onChange={e=>setPassword(e.target.value)} />
                     </div>
-                    <a
-                        href="#"
-                        className="text-xs text-indigo-600 hover:underline"
-                    >
-                        Forget Password?
-                    </a>
-                    <div className="mt-6">
+                   
+                    <div className="mt-12">
                         <button type='submit' className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-indigo-700 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600">
                             Login
                         </button>
