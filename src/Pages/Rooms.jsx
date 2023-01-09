80ac51608618dab45e992599d427c861d6546a54
Great Sage <michaelcull9715@gmail.com>
2023-01-09 15:45:52 +0000

    fc

--- src/Pages/Rooms.jsx
+++ src/Pages/Rooms.jsx
@@ -1,15 +1,22 @@
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import { useDemoData } from '@mui/x-data-grid-generator';
-
+import EmployeeDataService from '../Operations';
 const VISIBLE_FIELDS = ['name', 'rating', 'country', 'dateCreated', 'isAdmin'];
 
 export default function BasicExampleDataGrid() {
-  const { data } = useDemoData({
-    dataSet: 'Employee',
-    visibleFields: VISIBLE_FIELDS,
-    rowLength: 100,
-  });
+  
+  const [messages, setMessages] = React.useState([]);
+  React.useEffect(() => {
+    getAllMessages();
+  }, []);
+  const getAllMessages = async () => {
+    const {data} = await EmployeeDataService.getAllMessages(
+     { dataSet: 'messages',
+      visibleFields: VISIBLE_FIELDS,
+      rowLength: 100,}
+    );
+  };
 
   return (
     <div style={{ height: 400, width: '100%' }}>
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,4 +1,9 @@
 
 export { default as Dashboard } from './Dashboard';
+export { default as Bookings } from './Bookings';
+export { default as BookingsAdd } from './BookingsAdd';
+export { default as BookingsAllocate } from './BookingsAllocate';
+export { default as Dashboard } from './Dashboard';
+export { default as Dashboard } from './Dashboard';
 
 
